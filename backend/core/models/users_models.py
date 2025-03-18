9458851cdc0db90894013003605ac1aef2bf1308
LePie <fedorovkola97@gnail.com>
2025-03-18 17:24:56 +0300

    add enum role for users

--- backend/core/models/users_models.py
+++ backend/core/models/users_models.py
@@ -1,7 +1,19 @@
-from sqlalchemy import Column, Integer, String
 from .base import Base
+
+from sqlalchemy import Column, String, Enum  
+import enum
+
+
+''' Добавляем Enum роли '''
+class UserRole(enum.Enum):
+    READER = 'reader'
+    LIBRARIAN = 'librarian'
+    GUEST = 'guest'
+    ADMIN = 'admin'
+    
+    
 
 class User(Base):
     username = Column(String(50), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
-    role = Column(String(50), nullable=False)  
+    role = Column(Enum(UserRole), nullable=False) 
