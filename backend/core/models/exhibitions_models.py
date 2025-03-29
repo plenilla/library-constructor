bbf817656678952902d285fce486453703299484
Roman <e.roman.v@ya.ru>
2025-03-29 16:49:09 +0300

    Refactor exhibition models to use SQLAlchemy Column for attributes and improve relationship definitions

--- backend/core/models/exhibitions_models.py
+++ backend/core/models/exhibitions_models.py
@@ -1,77 +1,83 @@
-from sqlalchemy import ForeignKey, Text, Boolean, Column
-from sqlalchemy.orm import relationship, Mapped, mapped_column
-from typing import Optional, List
-
+from sqlalchemy import ForeignKey, Text, Boolean, Column, Integer
+from sqlalchemy.orm import relationship
+import enum
 
 from .base import Base
 
 
 class Exhibition(Base):
-    title: Mapped[str] = mapped_column(Text)
-    sections: Mapped[List["Section"]] = relationship(
-        back_populates="exhibitions", lazy="selectin", cascade="all, delete-orphan"
-    )
+    title = Column(Text, nullable=False)
     is_published = Column(Boolean, default=False)
+    sections = relationship(
+        "Section",
+        back_populates="exhibitions",
+        lazy="selectin",
+        cascade="all, delete-orphan",
+    )
 
 
 class Section(Base):
-    title: Mapped[str] = mapped_column(Text)
-    contents: Mapped[List["Content"]] = relationship(
-        back_populates="sections", lazy="selectin", cascade="all, delete-orphan"
+    title = Column(Text, nullable=False)
+    exhibition_id = Column(
+        Integer, ForeignKey("exhibitions.id", ondelete="CASCADE"), nullable=False
     )
-    exhibition_id: Mapped[int] = mapped_column(
-        ForeignKey("exhibitions.id", ondelete="CASCADE")
-    )
-    exhibitions: Mapped["Exhibition"] = relationship(
+    exhibitions = relationship(
         "Exhibition",
         back_populates="sections",
         lazy="selectin",
     )
+    contents = relationship(
+        "Content",
+        back_populates="sections",
+        lazy="selectin",
+        cascade="all, delete-orphan",
+    )
 
 
 class TextArray(Base):
-    text_data: Mapped[str] = mapped_column(Text)
-    contents_text: Mapped["Content"] = relationship(
+    text_data = Column(Text, nullable=False)
+    contents_text = relationship(
+        "Content",
         back_populates="text_data",
         lazy="selectin",
     )
 
 
 class Content(Base):
-    section_id: Mapped[int] = mapped_column(
-        ForeignKey("sections.id", ondelete="CASCADE")
+    section_id = Column(
+        Integer, ForeignKey("sections.id", ondelete="CASCADE"), nullable=False
     )
-    sections: Mapped["Section"] = relationship(
+    sections = relationship(
         "Section",
         back_populates="contents",
         lazy="selectin",
     )
 
-    books_id: Mapped[int] = mapped_column(
+    books_id = Column(
+        Integer,
         ForeignKey("books.id"),
         nullable=True,
     )
+    books = relationship("Book", back_populates="contents", lazy="selectin")
 
-    books: Mapped[Optional["Book"]] = relationship(
-        back_populates="contents", lazy="selectin"
-    )
-
-    text_id: Mapped[int] = mapped_column(
-        ForeignKey(TextArray.id),
+    text_id = Column(
+        Integer,
+        ForeignKey("textarrays.id"),  # Обратите внимание на имя таблицы
         nullable=True,
     )
-    text_data: Mapped[Optional["TextArray"]] = relationship(
+    text_data = relationship(
+        "TextArray",
         back_populates="contents_text",
         lazy="selectin",
     )
 
 
 class Book(Base):
-    title: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
-    description: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
-    bo: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
-    image: Mapped[str] = mapped_column(Text)
-    contents: Mapped["Content"] = relationship(
+    title = Column(Text, nullable=True)
+    description = Column(Text, nullable=True)
+    bo = Column(Text, nullable=True)
+    image = Column(Text, nullable=False)
+    contents = relationship(
         "Content",
         back_populates="books",
         lazy="selectin",
