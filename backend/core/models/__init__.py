b3c116c9c138488fe0bd946d116277e6f3abf590
Roman <e.roman.v@ya.ru>
2025-03-29 16:37:27 +0300

    Add async lifespan management for FastAPI and update model imports

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,7 +1,7 @@
 __all__ = (
     "Base",
     "Section",
-    "DatabaseHelper", 
+    "DatabaseHelper",
     "db_helper",
     "Content",
     "User",
@@ -16,3 +16,5 @@ from .base import Base
 from .db_helper import DatabaseHelper, db_helper, get_db
 from .exhibitions_models import Content, Section, TextArray, Book, Exhibition
 from .users_models import User, UserRole
+
+engine = db_helper.engine
--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,31 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.sql import text
+from contextlib import asynccontextmanager
 
 
 from app.middleware import setup_middleware
 from app.auth.auth_routers import router as users_router
 from app.constructor.exhibitions_routers import router as exhibitions_router
 from app.pages import router as page_router
-from core.models import get_db
+from core.models import get_db, Base, engine
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Код выполняется при запуске
+    async with engine.begin() as conn:
+        # Для создания всех таблиц
+        await conn.run_sync(Base.metadata.create_all)
+        print("Database tables created or already exist")
+
+    yield  # Здесь приложение работает
+
+    # Код выполняется при завершении
+    # Можно добавить очистку ресурсов
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 # Подключаем роутеры
