f9178c9e230f86851650bdacee6d0a373f22aeec
plenilla <fedorovkola2017@gmail.com>
2025-04-04 03:51:09 +0300

    fix cors

--- backend/app/middleware.py
+++ backend/app/middleware.py
@@ -4,11 +4,12 @@ from ..core.config import settings
 
 
 def setup_middleware(app):
-    app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
-
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
+        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
+    # Затем SessionMiddleware
+    app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
